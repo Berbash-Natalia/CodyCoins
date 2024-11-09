aa83a8fd9eff096f45b448886f3469362d71cda6
Berbash-Natalia <berbasn@gmail.com>
2024-11-09 17:23:39 +0300

    add final + sound

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import pgzrun
 from random import randint
 
+
+
 WIDTH = 400
 HEIGHT = 400
 
 cody = Actor('cody')
 coin = Actor('coin')
-bomb = Actor('bomb')
+cody.x=200
+cody.y=200
+coin.x = randint(10, 400)
+coin.y = randint(10, 400)
 score = 0
+
+game_over = False
+
+beep = tone.create('E3', 0.5)
+
+
 
 
 def draw():
-    global score
-    screen.clear()
-    screen.fill((132, 213, 0))
-    cody.draw()
-    coin.draw()
-    if score >= 40:
-        bomb.draw()
-    if cody.colliderect(bomb):
-        score -= 10
-        place_bomb()
-
-
-    screen.draw.text("Cody's coins = " + str(score), topleft=(10, 10), color="black", fontsize=30)
+    if game_over:
+        screen.fill((225,99,19))
+        screen.draw.text("The end! :( ", topleft=(10, 60), fontsize=60)
+        screen.draw.text("Cody's coins = : " + str(score), color="black", topleft=(10, 10), fontsize=60)
+    else:
+        screen.clear()
+        screen.fill((132, 213, 0))
+        cody.draw()
+        coin.draw()
+        screen.draw.text("Cody's coins = " + str(score), topleft=(10, 10), color="black", fontsize=30)
 
 
 def place_coin():
@@ -31,9 +39,9 @@ def place_coin():
     coin.y = randint(10, 400)
 
 
-def place_bomb():
-    bomb.x = randint(10, 400)
-    bomb.y = randint(10, 400)
+def the_end():
+    global game_over
+    game_over = True
 
 
 def update():
@@ -49,7 +57,8 @@ def update():
         cody.y += speed
     if cody.colliderect(coin):
         score += 10
+        beep.play()
         place_coin()
 
-
+clock.schedule(the_end,10)
 pgzrun.go()
